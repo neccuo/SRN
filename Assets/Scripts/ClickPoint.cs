d03193b55403da97b86aa784094677b8129fdc5c
Necati Mert Aydemir <nmaydemir@gmail.com>
2022-02-11 20:17:23 +0300

    Point&Click implementation is done

--- Assets/Scripts/ClickPoint.cs
+++ Assets/Scripts/ClickPoint.cs
@@ -7,31 +7,69 @@ public class ClickPoint : MonoBehaviour
     public Vector2 origin;
     public Vector2 target;
 
-    public GameObject cross;
+
+    public GameObject crossObject;
+    public float crossScale = 5f;
+
+    public GameObject dotObject;
+    public float dotScale = 1f;
+
+    List<Object> dotPointers;
+    Object crossPointer;
 
     // Start is called before the first frame update
     void Start()
     {
+        dotPointers = new List<Object>();
         origin = transform.position;
+
+        crossObject.transform.localScale = new Vector2(crossScale, crossScale);
+        dotObject.transform.localScale = new Vector2(dotScale, dotScale);
+
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButton(0))
+        if (Input.GetMouseButtonDown(0))
         {
             SpawnCursor();
+            SpawnDots();
         }
         
     }
 
+    void SpawnDots()
+    {
+        if(dotPointers.Count > 0)
+        {
+            for(int i = 0; i < dotPointers.Count; i++)
+            {
+                Destroy(dotPointers[i]);
+            }
+        }
+        
+        Vector2 difference = target - origin;
+        Vector2 unit = difference.normalized;
+
+
+        for(Vector2 parser = origin; parser.magnitude < target.magnitude; parser += unit)
+        {
+            dotPointers.Add(Instantiate(dotObject, parser, new Quaternion(0, 0, 0, 0)));
+        }
+    }
+
     void SpawnCursor()
     {
+        Destroy(crossPointer);
         target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        Object abc = Instantiate(cross, target, new Quaternion(0, 0, 0, 0));
-        Debug.Log("" + abc.ToString() + " is created");
+        Vector2 dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        crossPointer = Instantiate(crossObject, target, Quaternion.AngleAxis(angle - 90, Vector3.forward));
+        Debug.Log("" + crossPointer.ToString() + " is created");
 
     }
 }
