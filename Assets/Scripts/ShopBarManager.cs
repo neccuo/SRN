c2ae565eb9767606e97677d5be652d134195cfbc
Necati Mert Aydemir <nmaydemir@gmail.com>
2022-03-05 20:17:51 +0300

    Introduction to ShopBar

---
+++ Assets/Scripts/ShopBarManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopBarManager : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Debug.Log("Shop Bar is Enabled");
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("Shop Bar is Disabled");
+    }
+}
