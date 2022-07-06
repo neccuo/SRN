d9d7b5fa87aea8b4c7b71759a954a0d740246bf0
Necati Mert Aydemir <mert.aydemir@mynet.games>
2022-07-07 00:21:49 +0300

    portal refactor & foundations

--- Assets/Scripts/Space Objects/PortalLogic.cs
+++ Assets/Scripts/Space Objects/PortalLogic.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class PortalLogic : MonoBehaviour
 {
+    [SerializeField] private int _boundSystemID; // Maybe check DB for available IDs to error check
     private void OnTriggerEnter2D(Collider2D col)
     {
         GameObject collidedObject = col.gameObject;
         Debug.Log("" + collidedObject.name + " collided with " + this.name);
         if(collidedObject.tag == "NPC" && collidedObject.GetComponent<NPC>().teleportReady)
         {
-            // Debug.Log("You can teleport");
             
             // MAKE SURE TO ADD SOMETHING USEFUL HERE
             Destroy(collidedObject);
--- Assets/Scripts/Space Objects/PortalManager.cs
+++ Assets/Scripts/Space Objects/PortalManager.cs
@@ -12,25 +12,10 @@ public class PortalManager : MonoBehaviour
     {
         Debug.Log("we are going to be rich!!!");
 
-        // SHITTIEST HACK, DEFINITELY WILL CHANGE IT
-        /*DontDestroyOnLoad(GameObject.Find("Game Manager"));
-        DontDestroyOnLoad(GameObject.Find("ClickPoint"));
-        DontDestroyOnLoad(GameObject.Find("Main Camera"));
-        DontDestroyOnLoad(GameObject.Find("main_space"));
-        DontDestroyOnLoad(GameObject.Find("Player"));
-        DontDestroyOnLoad(GameObject.Find("Canvas"));
-        DontDestroyOnLoad(GameObject.Find("EventSystem"));
-
-        SceneManager.CreateScene("asfasdgasdg");
-        SceneManager.LoadScene("asfasdgasdg");*/
-
-        //Destroy(GameObject.Find("Planets"));
-        //Destroy(GameObject.Find("NPC"));
 
         _DestroyChildren(GameObject.Find("Planets"));
         _DestroyChildren(GameObject.Find("NPCs"));
 
-        //Destroy(GameObject.Find("Sun")); // MAYBE CHANGE ANIMATION SET, OCCURENCES
 
 
         GameObject player = GameObject.Find("Player");
@@ -39,6 +24,11 @@ public class PortalManager : MonoBehaviour
         player.transform.position = new Vector3(-pos.x, -pos.y, pos.z);
 
         Controller.ControllerGod.ChangeState(GameState.PlanMovement);
+    }
+
+    private void _DisableUnusedChildren(GameObject gObject)
+    {
+        // TODO: WITH SimpleDB, get
     }
 
     private void _DestroyChildren(GameObject gobject) // DESTORYS CHILDREN BUT NOT THE PARENT
